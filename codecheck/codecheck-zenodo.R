7bb1df2f87ba8c46ad81e3bc1483cdba4594aec2
Stephen J. Eglen <sje30@cam.ac.uk>
2020-07-16 10:48:26 +0100

    revise layout of the zenodo upload

--- codecheck/codecheck-zenodo.R
+++ codecheck/codecheck-zenodo.R
@@ -1,34 +1,53 @@
 ## Zenodo deposit; see vignette("codecheck_overview.Rmd")
+## https://github.com/codecheckers/notes/blob/master/codechecker-instructions.md
+## Run this code from the codecheck/ folder.  But read it carefully
+## rather than simply running the whole script.
+
 
 library("codecheck")
 
-## This assumes your working directory is the codecheck directory
-metadata = read_yaml( "../codecheck.yml") 
 
-## To interact with the Zenodo API, you need to create a token.  This should
-## not be shared, or stored in this script.  Here I am using the Unix password
-## tool pass to retrieve the token.
-my_token = system("pass show codechecker-token", intern=TRUE)
+## Part one: create a new record ---------------------------------------
+## You need do this only once for the certificate.
+
+## To interact with the Zenodo API, you need to create a token.  This
+## should not be shared, or stored in this script.  Uncomment one of
+## the two following lines to get your token stored in `my_token`.
+## my_token = system("pass show codechecker-token", intern=TRUE)
+## my_token = readLines("~/zenodo-token.txt")
+
 
 ## make a connection to Zenodo API
 zenodo <- ZenodoManager$new(token = my_token)
 
 
-## If you wish to create a new record on zenodo, run the following line once
-## and then store the URL of the record in  ../codecheck.yml
 ## This will generate a new record every time that you run it and 
 ## save the new record ID in the codecheck configuration file:
-## record = create_zenodo_record(zenodo); metadata = read_yaml( "../codecheck.yml")
+record = create_zenodo_record(zenodo)
+
+
+
+## Part two: upload the metadata --------------------------------------
+## You may run this several times if you edit the metadata.
+
+metadata = read_yaml( "../codecheck.yml")
 
 record = get_zenodo_record(metadata$report)
 codecheck:::set_zenodo_metadata(zenodo, record, metadata)
 
+
+
+
+## Part three: upload the certificate ---------------------------------
 ## If you have already uploaded the certificate once, you will need to
 ## delete it via the web page before uploading it again.
-## codecheck:::set_zenodo_certificate(zenodo, record, "codecheck.pdf") 
+
+codecheck:::set_zenodo_certificate(zenodo, record, "codecheck.pdf")
 
 ## You may also create a ZIP archive of of any data or code files that
 ## you think should be included in the CODECHECK's record.
+
+
 
 ## Now go to zenodo and check the record (the URL is printed
 ## by set_zenodo_metadata() ) and then publish.
